4b55b9cf5748666d0c824e5bfb7baa5071029305
Ayxan Mustafayev <118119951+ayxansumqayit@users.noreply.github.com>
2023-08-28 19:56:53 +0500

    Courses

---
+++ application/controllers/User_controller.php
@@ -0,0 +1,13 @@
+<?php
+
+class User_controller extends CI_Controller{
+
+    public function index(){
+        echo "Men index metoduyam";
+    }
+
+    public function about(){
+        echo "Men about metoduyam";
+    }
+
+}
